66c8c7bd5a46f99199d87f8bd5e2e6cf57dfc2ed
Sakdinon Piakat <dizarale@users.noreply.github.com>
2016-03-10 00:08:22 +0700

    Update food-app.js

--- food-app.js
+++ food-app.js
@@ -6,7 +6,7 @@ var cookie  = require('cookie-parser');
 var mongo   = require('mongodb').MongoClient;
 var storage = 'mongodb://localhost:27017/food-app';
 
-var page = ['index','user','resturant','menu'];
+var page = ['index','user','resturant','menu','preorder'];
 
 
 app.use(bodyParser.urlencoded({ extended: true  }));
@@ -15,31 +15,7 @@ app.use(bodyParser.text({ type: 'text/html' }));
 app.use(cookie());
 app.use(express.static('public'));
 
-app.post('/user', (req, res) => {
-	mongo.connect(storage, (error, database) => {
-		database
-		.collection('users')
-		.find({email:req.body.email})
-		.toArray((error, result) => {
-			if(error){
-				let result = { "status" : 0 , "detail" : "Database Error" };
-				res.json(result);
-			}else if (result.length == 0) {
-				database.collection('users').insert({
-					email:    req.body.email,
-					token:    req.body.token,
-					pic: 	  req.body.pic,
-					public_profile : req.body.public_profile
-				});
-				let result = { "status" :  1 , "detail" : "insert success" };
-				res.json(result);
-			}else if(result.length == 1){
-				let result = { "status" : -1 , "detail" : "Duppicate User" };
-				res.json(result);
-			}
-		});
-	});
-});
+
 app.get ('/user',  (req, res) => {
 	mongo.connect(storage, (error, database) => {
 		database
@@ -73,8 +49,49 @@ app.get ('/user/:email',  (req, res) => {
 		});
 	});
 });
+app.post('/user', (req, res) => {
+	mongo.connect(storage, (error, database) => {
+		database
+		.collection('users')
+		.find({email:req.body.email})
+		.toArray((error, result) => {
+			if(error){
+				let result = { "status" : 0 , "detail" : "Database Error" };
+				res.json(result);
+			}else if (result.length == 0) {
+				database.collection('users').insert({
+					email:    req.body.email,
+					token:    req.body.token,
+					pic: 	  req.body.pic,
+					public_profile : req.body.public_profile
+				});
+				let result = { "status" :  1 , "detail" : "insert success" };
+				res.json(result);
+			}else if(result.length == 1){
+				let result = { "status" : -1 , "detail" : "Duppicate User" };
+				res.json(result);
+			}
+		});
+	});
+});
 app.get ('/resturant',  (req, res) => {
-	let user_email = req.params.email
+	mongo.connect(storage, (error, database) => {
+		database
+		.collection('resturant')
+		.find().
+		toArray((error, result) => {
+			if (result.length == 0) {
+				let result = { "status" : -1, "detail" : "Not Found Resturant"};
+				res.json(result);
+			}else{
+				let data = { "status":1, "detail" : "Found Resturant", "row" : result.length, "data":result}
+				res.json(data);
+			}
+		});
+	});
+});
+app.get ('/resturant/rout',  (req, res) => {
+	let res_id = require('mongodb').ObjectID("56e05344f4e190cb06f67b28");
 	mongo.connect(storage, (error, database) => {
 		database
 		.collection('resturant')
@@ -107,6 +124,7 @@ app.get ('/resturant/:res_id',  (req, res) => {
 		});
 	});
 });
+
 app.post('/resturant', (req, res) => {
 	mongo.connect(storage, (error, database) => {
 		database
@@ -117,9 +135,19 @@ app.post('/resturant', (req, res) => {
 				let result = { "status" : 0 , "detail" : "Database Error" };
 				res.json(result);
 			}else if (result.length == 0) {
+				var location = []
+				location.push({row:0,data:[]});
 				database.collection('resturant').insert({
 					res_name:    req.body.res_name,
-					res_location:{ lat : req.body.lat , lng : req.body.lng}
+					res_img:     req.body.res_img,
+					res_detail:  req.body.res_detail,
+					res_manager: req.body.res_manager,
+					res_tel:     req.body.res_tel,
+					res_email:   req.body.res_email,
+					res_open:    req.body.res_open,
+					res_close:    req.body.res_close,
+					res_status:    req.body.res_status,
+					res_location: location
 				});
 				let result = { "status" :  1 , "detail" : "insert success" };
 				res.json(result);
@@ -140,15 +168,103 @@ app.put('/resturant', (req, res) => {
 				let result = { "status" : 0 , "detail" : "Database Error" };
 				res.json(result);
 			}else if (result.length == 1) {
-				database.collection('resturant').update({res_name:req.body.res_name},{
+				var location = []
+				location.push({row:0,data:[]});
+				database.collection('resturant').update({res_name:req.body.res_name},{ $set:{
 					res_name:    req.body.res_name,
-					res_location:{ lat : req.body.lat , lng : req.body.lng}
-				});
+					res_img:     req.body.res_img,
+					res_detail:  req.body.res_detail,
+					res_manager: req.body.res_manager,
+					res_tel:     req.body.res_tel,
+					res_email:   req.body.res_email,
+					res_open:    req.body.res_open,
+					res_close:    req.body.res_close,
+					res_status:    req.body.res_status,
+					res_location: location
+				}},
+				{ upsert: true }
+				);
 				let result = { "status" :  1 , "detail" : "Update success" };
 				res.json(result);
 			}else if(result.length == 0){
 				let result = { "status" : -1 , "detail" : "Not Found Resturant" };
 				res.json(result);
+			}
+		});
+	});
+});
+app.post('/resturant/addarea/:res_id', (req, res) => {
+	let res_id = require('mongodb').ObjectID(req.params.res_id);
+	mongo.connect(storage, (error, database) => {
+		database
+		.collection('resturant')
+		.find({ "_id" : res_id }).
+		toArray((error, result) => {
+			if (result.length == 0) {
+				let result = { "status" : -1, "detail" : "Not Found Resturant"};
+				res.json(result);
+			}else{
+				var location = {};
+				var location_data = [];
+				var num = result[0]['res_location']['row'];
+				
+				if(num>0){
+					for(var i = 0 ; i< num ; i++){
+						location_data.push(result[0]['res_location']['data'][i]);
+					}
+				}
+				console.log("location_data :"+ JSON.stringify(location_data));
+				let location_name = req.body.location_name;
+				let LTlat = req.body.LTlat;
+				let RTlat = req.body.RTlat;
+				let LTlng = req.body.LTlnt;
+				let RTlng = req.body.RTlng;
+				let LBlat = req.body.LBlat;
+				let RBlat = req.body.RBlat;
+				let LBlng = req.body.LBlng;
+				let RBlng = req.body.RBlng;
+				let location_cost = req.body.cost;
+				var location_point = {LTLat:LTlat,RTLat:RTlat,LTLng:LTlng,RTLng:RTlng,LBLat:LBlat,RBLat:RBlat,LBLng:LBlng,RBLng:RBlng};
+				var location_area = {}
+				if(num>0){
+					num++;
+					location_area = {area :num ,name : location_name,cost : location_cost,location_latlng:location_point};
+				}else{
+					num = 1;
+					location_area = {area :num ,name : location_name,cost : location_cost,location_latlng:location_point};
+				}
+				console.log("location_point :"+ JSON.stringify(location_point));
+				console.log("location_area :"+ JSON.stringify(location_area));
+
+				location_data.push(location_area);
+				console.log("location_data :"+ JSON.stringify(location_data));
+
+				location = {row:num,data:location_data};
+				console.log("location :"+ JSON.stringify(location));
+				database.collection('resturant').update({ "_id" : res_id },{ $set:{
+					res_location: location
+				}}
+				);
+				let data = { "status":1, "detail" : "Update Location Success"};
+				res.json(data);
+			}
+		});
+});
+});
+app.post('/resturant/editarea/:res_id', (req, res) => {
+	let res_id = require('mongodb').ObjectID(req.params.res_id);
+	mongo.connect(storage, (error, database) => {
+		database
+		.collection('resturant')
+		.find({ "_id" : res_id }).
+		toArray((error, result) => {
+			if (result.length == 0) {
+				let result = { "status" : -1, "detail" : "Not Found Resturant"};
+				res.json(result);
+			}else{
+				
+				let data = { "status":1, "detail" : "Update Location Success"};
+				res.json(data);
 			}
 		});
 	});
@@ -200,7 +316,8 @@ app.post('/menu/:res_id', (req, res) => {
 							res_id:   res_id,
 							name:     req.body.name,
 							price:    req.body.price,
-							pic: 	  req.body.pic
+							pic: 	  req.body.pic,
+							type: 	  req.body.type
 						});
 						let result = { "status" :  1 , "detail" : "insert success" };
 						res.json(result);
@@ -236,7 +353,8 @@ app.put('/menu/:res_id', (req, res) => {
 							res_id:   res_id,
 							name:     req.body.name,
 							price:    req.body.price,
-							pic: 	  req.body.pic
+							pic: 	  req.body.pic,
+							type: 	  req.body.type
 						});
 						let result = { "status" :  1 , "detail" : "Update Menu Success" };
 						res.json(result);
@@ -246,61 +364,106 @@ app.put('/menu/:res_id', (req, res) => {
 		});
 	});
 });
+app.get ('/order/preorder/',  (req, res) => {
+	mongo.connect(storage, (error, database) => {
+		database
+		.collection('orders')
+		.find().
+		toArray((error, result) => {
+			if (result.length == 0) {
+				let result = { "status" : -1, "detail" : "Not Found Order"};
+				res.json(result);
+			}else{
+				let data = { "status":1, "detail" : "Found Order", "row" : result.length, "data":result}
+				res.json(data);
+			}
+		});
+	});
+});
+app.post('/order/preorder/menu', (req, res) => {
+	var user = req.body.email;
+	let res_id =  req.body.res_id;
+	let Menu_id = req.body.menu_id;
+	let Menu_num = req.body.menu_num;
+	let Menu_des = req.body.menu_des;
+	let menu = {menu_id : Menu_id , menu_num:Menu_num , menu_des : Menu_des};
+	let order_lat = req.body.order_lat;
+	let order_lng = req.body.order_lng;
+	let order_detail = req.body.order_detail;
+
+	if(Menu_num>0 && Menu_num<100){
+		mongo.connect(storage, (error, database) => {
+			database
+			.collection('users')
+			.find({email:user})
+			.toArray((error, result) => {
+				if(error){
+					let result = { "status" : 0 , "detail" : "Database Error" };
+					res.json(result);
+				}else if (result.length == 0) {
+					let result = { "status" :  -1 , "detail" : "Not Have User" };
+					res.json(result);
+				}else if(result.length == 1){
+					database.collection('orders').find({email:user,order_status:0}).toArray((error,result) =>{
+						if(error){
+							let result = { "status" : 0 , "detail" : "Database Error" };
+							res.json(result);
+						}else if(result.length == 0){
+							var order = [];
+							order.push(menu);
+							var order_menu = { row:1 , menu : order};
+							database.collection('orders').insert({
+								email:    user,
+								order_status : 0,
+								order_location : { order_lat : order_lat , order_lng : order_lng },
+								order_detail : order_detail,
+								order_menu : order_menu
+							});
+							let result = { "status" : 0 , "detail" : "Insert Pre-Order" };
+							res.json(result);
+						}else if(result.length == 1){
+							var order = [];
+							let num = result[0]['order_menu']['row'];
+							var check = false;
+							for (var i = 0; i < num; i++) {
+								if(Menu_id == result[0]['order_menu']['menu'][i]['menu_id'] && Menu_des == result[0]['order_menu']['menu'][i]['menu_des']){
+									console.log("have");
+									result[0]['order_menu']['menu'][i]['menu_num'] = parseInt(result[0]['order_menu']['menu'][i]['menu_num']) +  parseInt(Menu_num);
+									order.push(result[0]['order_menu']['menu'][i]);
+									check = true;
+								}else{
+									order.push(result[0]['order_menu']['menu'][i]);
+								}
+							};
+							var data;
+							if(check){
+								var order_menu = { row:num , menu : order};
+								data = { "status" : 1 , "detail" : "Update Menu in Pre-Order" };
+							}else{
+								num++;
+								var order_menu = { row:num , menu : order};
+								order.push(menu);
+								data = { "status" : 1 , "detail" : "Update Insert Menu in Pre-Order" };
+							}
+							database.collection('orders').update({ email:user },{ $set:{
+								order_menu : order_menu
+							}});
+							
+							res.json(data);
+						}
+					});
+				}
+			});
+		});
+	}else{
+		var data;
+		data = { "status" : -1 , "detail" : "menu is 0 or Over [Who are You]" };
+	}
+});
 app.use(ErrorHandler);
 app.listen(1200);
 
 function ErrorHandler( req, res) {
-	var check =0;
-	var result="";
-	for (var v of page){
-		var test = longestSubstring(req.url,v);
-		if(test == check){
-			result+=" or "+v;
-			check=test;
-		}
-		if(test > check){
-			result=v;
-			check=test;
-		}
-	}
-	res.status(404).send('error 404 Page not found!!'+'  Are You Mean '+result);
+	res.status(404).send('error 404 Page not found!!');
 }
-function longestSubstring(s,t){
-	var reS = t;
-	var ans = "";
-	var textcheck ="";
-	var count=0;
-	var anscount=0;
-	var con=false;
-	if(s==reS) return s;
-	var i=0;
-	var j=0;
-	var n = s.length-1;
-    	while(i<=s.length-1&&j<=s.length-1){
-    		if(reS[j]==s[i]&&con){
-    			count++;
-    			textcheck+=s[i];
-    			j++;
-    			i++;
-    		}else if(reS[j]==s[i]){
-	    		count = 1;
-	    		textcheck = reS[j];
-	    		j++;
-	    		con = true;
-	    		i++;
-	    		
-	    	}else if(con){
-	    		con =false;
-	    		i=0;
-	    		//console.log(anscount+":"+ans+":"+count);
-	    	}else{
-	    		i++;
-	    	}
-	    }
-	    if(anscount<count){
-	    			anscount = count;
-	    			ans = textcheck;
-	    }
-	var percen = ans.length/s.length;
-    return percen;
-}
+
